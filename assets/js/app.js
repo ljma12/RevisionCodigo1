b32288e3565bb859a95683ea3da0aa9f7047ecfb
LJ Melendez <luisjosemelendezarvizu@gmail.com>
2024-09-26 12:15:13 -0600

    revision2

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,25 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
+const $n = document.querySelector('.name'); //corregi la llamada a name
 const $b = document.querySelector('#blog');
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+async function displayUser(username) { //se hace la funcion async
+  try{ //uso de try y catch
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  const data = await response.json();
+  $n.textContent = `${data.name}`;
+  $b.textContent = `${data.blog}`;
+  $l.textContent = `${data.location}`; //uso de comillas invertidas
+} catch (err) {
+  handleError(err);
+  }
 }
-
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}`; //se agrego punto y coma y $
 }
 
 displayUser('stolinski').catch(handleError);
